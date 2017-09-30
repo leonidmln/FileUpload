1556934bcb289f650c45308ae47acfefbcb42617
Leonid Molochniy <leonid.molochniy@gmail.com>
2017-09-30 08:23:41 +0300

    Add project files.

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using FileUpload.Filters;
+using FileUpload.Helpers;
+using FileUpload.Models;
+
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly FormOptions _defaultFormOptions = new FormOptions();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [DisableFormValueModelBinding]
+        public async Task<IActionResult> UploadFile()
+        {
+            string DirectoryPath = ".\\files";
+            string fileName = "";
+            
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+
+            // Used to accumulate all the form url encoded key value pairs in the 
+            // request.
+            var formAccumulator = new KeyValueAccumulator();
+            string targetFilePath = null;
+
+            var boundary = MultipartRequestHelper.GetBoundary(
+                MediaTypeHeaderValue.Parse(Request.ContentType),
+                _defaultFormOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+
+            var section = await reader.ReadNextSectionAsync();
+            while (section != null)
+            {
+                ContentDispositionHeaderValue contentDisposition;
+                var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    if (MultipartRequestHelper.HasFileContentDisposition(contentDisposition))
+                    {
+                        if(!System.IO.Directory.Exists(DirectoryPath))
+                        {
+                            System.IO.Directory.CreateDirectory(DirectoryPath);
+                        }
+
+                        fileName = Guid.NewGuid().ToString("N") + System.IO.Path.GetExtension(HeaderUtilities.RemoveQuotes(contentDisposition.FileName).Value);
+                        targetFilePath = DirectoryPath + "\\" + fileName;
+                        using (var targetStream = System.IO.File.Create(targetFilePath))
+                        {
+                            await section.Body.CopyToAsync(targetStream);
+
+                        }
+                    }
+                    else if (MultipartRequestHelper.HasFormDataContentDisposition(contentDisposition))
+                    {
+                        // Content-Disposition: form-data; name="key"
+                        //
+                        // value
+
+                        // Do not limit the key name length here because the 
+                        // multipart headers length limit is already in effect.
+                        var key = HeaderUtilities.RemoveQuotes(contentDisposition.Name);
+                        var encoding = GetEncoding(section);
+                        using (var streamReader = new StreamReader(
+                            section.Body,
+                            encoding,
+                            detectEncodingFromByteOrderMarks: true,
+                            bufferSize: 1024,
+                            leaveOpen: true))
+                        {
+                            // The value length limit is enforced by MultipartBodyLengthLimit
+                            var value = await streamReader.ReadToEndAsync();
+                            if (String.Equals(value, "undefined", StringComparison.OrdinalIgnoreCase))
+                            {
+                                value = String.Empty;
+                            }
+                            formAccumulator.Append(key.Value, value);
+
+                            if (formAccumulator.ValueCount > _defaultFormOptions.ValueCountLimit)
+                            {
+                                throw new InvalidDataException($"Form key count limit {_defaultFormOptions.ValueCountLimit} exceeded.");
+                            }
+                        }
+                    }
+                }
+
+                // Drains any remaining section body that has not been consumed and
+                // reads the headers for the next section.
+                section = await reader.ReadNextSectionAsync();
+            }
+
+            // Bind form data to a model
+            var formValueProvider = new FormValueProvider(
+                BindingSource.Form,
+                new FormCollection(formAccumulator.GetResults()),
+                CultureInfo.CurrentCulture);
+
+
+            return Json(new { state = 0, file_name = fileName });
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static Encoding GetEncoding(MultipartSection section)
+        {
+            MediaTypeHeaderValue mediaType;
+            var hasMediaTypeHeader = MediaTypeHeaderValue.TryParse(section.ContentType, out mediaType);
+            // UTF-7 is insecure and should not be honored. UTF-8 will succeed in 
+            // most cases.
+            if (!hasMediaTypeHeader || Encoding.UTF7.Equals(mediaType.Encoding))
+            {
+                return Encoding.UTF8;
+            }
+            return mediaType.Encoding;
+        }
+    }
+}
---
+++ FileUpload/readme.txt
@@ -0,0 +1,20 @@
+﻿if you use everything should be ok,
+but if you use IIS or IIS Express
+you should to change web.config, owerthise you will see error 
+
+HTTP 404.13 - Not Found
+The request filtering module is configured to deny a request that exceeds the request content length.
+
+IIS - change web.config
+<system.webServer>
+  <security>
+    <requestFiltering>
+      <!-- This will handle requests up to 50MB -->
+      <requestLimits maxAllowedContentLength="52428800" />
+    </requestFiltering>
+  </security>
+</system.webServer>
+
+IIS Express - you should change applicationhost.config
+how to find applicationhost.config if you use IIS Express
+https://stackoverflow.com/questions/12946476/where-is-the-iis-express-configuration-metabase-file-found
